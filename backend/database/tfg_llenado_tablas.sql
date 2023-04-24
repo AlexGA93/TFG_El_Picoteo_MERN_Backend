7446922b0a711437a8bea24dd17c7f8e40f752c6
AlexHA93 <alexgimat@hotmail.com>
2023-04-24 09:05:54 +0200

    update database and dependencies

--- backend/database/tfg_llenado_tablas.sql
+++ backend/database/tfg_llenado_tablas.sql
@@ -332,6 +332,20 @@ ponemos en stoock. En este caso cada ingrediente tendra su (-)(numero de ingredi
 (57, 1*(-5), now()) --mayonesa
 ;
 
+-- Cuando se hace una compra debemos registrar en stock la cuenta negativa.
+-- EN ESTE CASO HEMOS VENDIDO 1 MIXTO, 2 VEGETALES, 2 CHIVITOS
+
+INSERT INTO Stock
+(id_receta, unidades, fecha)
+VALUES
+-- mixto [id_receta: 1, unidades: 3]
+(1,3-1,now()),
+-- vegetal [id_receta: 3, unidades: 2]
+(3,2-2,now()),
+-- chivito [id_receta: 4, unidades: 5]
+(4,5-2,now())
+;
+
 /* ****************************** VISIOANDO DE TABLAS DE BASE DE DATOS ****************************** */
 
 SELECT * FROM el_picoteo.almacen;
