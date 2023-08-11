a9b9b85d835d7e3c6d8e558815d227e524d0f40f
Alejandro Gimeno <alexgimat@hotmail.com>
2023-08-11 12:11:46 +0200

    Authentication by roles

--- Backend/src/routes/auth.routes.ts
+++ Backend/src/routes/auth.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { login, registerUser } from "../controllers";
 import { check } from "express-validator";
+import { authenticationByAdmin } from "../auth/auth";
 const router: Router = Router();
 
 const passRegex: RegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
@@ -18,28 +19,46 @@ const passRegex: RegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
  * *[A-Z] -> zero or more times any character between [A-Z]
  * [0-9a-zA-Z] -> following characters must be any of these characters
  * {8,}$  -> 8 or more characters at the end
- * 
- * 
+ *
+ *
  * example: "92johnDOE4ever"
  */
 
+const emailRegex: RegExp = /^[A-Za-z0-9._%+-]+@elpicoteo\.com$/;
 
-router.post("/register", 
-  check('email').isEmail().withMessage("Email must have a valid format"),
-  check("password").isLength({ min: 6 }).withMessage("Password must be longer than 6 characters"),
-  check('password').matches(passRegex, "i").withMessage("Please enter a valid password"),
-  check('role').isString().withMessage("Role must have a valid value"),
-  check('role').custom((value) => value==='admin' || value==='employee').withMessage("Role must be a valid one")
-,registerUser);
-
+// PROTECTED ROUTE BY ADMIN
+router.post(
+  "/register",
+  check("email").isEmail().withMessage("Email must have a valid format"),
+  check("email")
+    .matches(emailRegex, "i")
+    .withMessage("Email must allow to the correct domain"),
+  check("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be longer than 6 characters"),
+  check("password")
+    .matches(passRegex, "i")
+    .withMessage("Please enter a valid password"),
+  check("role").isString().withMessage("Role must have a valid value"),
+  check("role")
+    .custom((value) => value === "admin" || value === "employee")
+    .withMessage("Role must be a valid one"),
+  registerUser
+);
 
 router.post(
   "/login",
-  check('email').isEmail().withMessage("Email must have a valid format"),
-  check("password").isLength({ min: 6 }).withMessage("Password must be longer than 6 characters"),
-  check('password').matches(passRegex, "i").withMessage("Please enter a valid password"),
+  check("email").isEmail().withMessage("Email must have a valid format"),
+  check("email")
+    .matches(emailRegex, "i")
+    .withMessage("Email must allow to the correct domain"),
+  check("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be longer than 6 characters"),
+  check("password")
+    .matches(passRegex, "i")
+    .withMessage("Please enter a valid password"),
   login
 );
-
 
 export default router;
--- Backend/src/routes/database.routes.ts
+++ Backend/src/routes/database.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { checkDDBB, getDatabaseTables, createTable } from "../controllers/database.controller";
+import { authenticationByAdmin } from "../auth/auth";
 const router: Router = Router();
 
-router.get("/", checkDDBB);
+router.get("/", authenticationByAdmin, checkDDBB);
 router.get("/:database_name/tables", getDatabaseTables);
 router.post("/:database_name/tables", createTable);
 
